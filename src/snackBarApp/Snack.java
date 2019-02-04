6bf516e9a2be9513763d1c65243469aafb993e44
Danny Vail <danny_vail@outlook.com>
2019-02-04 17:02:30 -0500

    CREATE SNACK CLASS

---
+++ src/snackBarApp/Snack.java
@@ -0,0 +1,42 @@
+package snackBarApp;
+
+public class Snack {
+  private static int maxId = 0;
+  private int id;
+  private String name;
+  private int quantity;
+  private double cost;
+  private int vendingMachineId;
+
+  public Snack(String name, int quantity, double cost, int vendingMachineId) {
+    maxId++;
+    id = maxId;
+
+    this.name = name;
+    this.quantity = quantity;
+    this.cost = cost;
+    this.vendingMachineId = vendingMachineId;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void addQuantity(int quantity) {
+    this.quantity += quantity;
+  }
+
+  public void buySnack(int quantity) {
+    this.quantity -= quantity;
+  }
+
+  public double getTotalCost(int quantity) {
+    double total = quantity * this.cost;
+
+    return total;
+  }
+}
