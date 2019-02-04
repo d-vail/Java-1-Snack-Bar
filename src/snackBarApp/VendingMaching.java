aa98d41757dd13537d0e251989a1ba73104fd934
Danny Vail <danny_vail@outlook.com>
2019-02-04 17:08:18 -0500

    CREATE VENDINGMACHINE CLASS

---
+++ src/snackBarApp/VendingMaching.java
@@ -0,0 +1,22 @@
+package snackBarApp;
+
+public class VendingMachine {
+  private static int maxId = 0;
+  private int id;
+  private String name;
+
+  public VendingMachine(String name) {
+    maxId++;
+    id = maxId;
+
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
