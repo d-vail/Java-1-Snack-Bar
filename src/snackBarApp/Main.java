f7eae6d1852c51b8b073dd1cb26d0712f27b53eb
Danny Vail <danny_vail@outlook.com>
2019-02-05 03:16:22 -0500

    REFACTOR MAIN CLASS In Main.java - Delete object printouts; were for testing - Remove unnecessary comments; repeats print logs - Utilize new Customer.buy method - Create a print helper method to log customer cash on hand and snack qtys - Fix vague variable names

--- src/snackBarApp/Main.java
+++ src/snackBarApp/Main.java
@@ -1,104 +1,60 @@
 package snackBarApp;
 
 public class Main {
+  private static void print(Customer customer, Snack snack) {
+    if (customer != null) {
+      System.out.println("Customer " + customer.getId() + " | Name: " + customer.getName() + ", Cash On Hand: $"
+          + customer.getCashOnHand());
+    }
+
+    if (snack != null) {
+      System.out.println("Snack " + snack.getId() + " | Name: " + snack.getName() + ", Qty: " + snack.getQuantity());
+    }
+
+    System.out.println("\n");
+  }
+
   private static void operateSnackBar() {
-    // Instantiate objects
-    Customer c1 = new Customer("Jane", 45.25);
-    Customer c2 = new Customer("Bob", 33.14);
+    Customer jane = new Customer("Jane", 45.25);
+    Customer bob = new Customer("Bob", 33.14);
 
-    VendingMachine v1 = new VendingMachine("Food");
-    VendingMachine v2 = new VendingMachine("Drink");
-    VendingMachine v3 = new VendingMachine("Office");
+    VendingMachine food = new VendingMachine("Food");
+    VendingMachine drink = new VendingMachine("Drink");
+    VendingMachine office = new VendingMachine("Office");
 
-    Snack s1 = new Snack("Chips", 36, 1.75, v1.getId());
-    Snack s2 = new Snack("Chocolate Bar", 36, 1.00, v1.getId());
-    Snack s3 = new Snack("Pretzel", 30, 2.00, v1.getId());
-    Snack s4 = new Snack("Soda", 24, 2.50, v2.getId());
-    Snack s5 = new Snack("Water", 20, 2.75, v2.getId());
+    Snack chips = new Snack("Chips", 36, 1.75, food);
+    Snack chocolateBar = new Snack("Chocolate Bar", 36, 1.00, food);
+    Snack pretzel = new Snack("Pretzel", 30, 2.00, food);
+    Snack soda = new Snack("Soda", 24, 2.50, drink);
+    Snack water = new Snack("Water", 20, 2.75, drink);
 
-    System.out.println("*** Starting Data ***");
-    System.out.println(c1);
-    System.out.println(c2);
-    System.out.println(v1);
-    System.out.println(v2);
-    System.out.println(v3);
-    System.out.println(s1);
-    System.out.println(s2);
-    System.out.println(s3);
-    System.out.println(s4);
-    System.out.println(s5);
-    System.out.println("\n");
-
-    /**
-     * Customer 1 buys 3 of snack 4. Print Customer 1 Cash on hand. Print quantity
-     * of snack 4.
-     */
     System.out.println("*** Customer 1 buys 3 of snack 4 ***");
-    c1.buy(s4.getTotalCost(3));
-    s4.buySnack(3);
-    System.out.println("Customer 1 Cash on Hand: " + c1.getCashOnHand());
-    System.out.println("Snack 4 Quantity: " + s4.getQuantity());
-    System.out.println("\n");
+    jane.buy(3, soda);
+    print(jane, soda);
 
-    /**
-     * Customer 1 buys 1 of snack 3. Print Customer 1 Cash on hand. Print quantity
-     * of snack 3.
-     */
     System.out.println("*** Customer 1 buys 1 of snack 3 ***");
-    c1.buy(s3.getTotalCost(1));
-    s3.buySnack(1);
-    System.out.println("Customer 1 Cash on Hand: " + c1.getCashOnHand());
-    System.out.println("Snack 3 Quantity: " + s3.getQuantity());
-    System.out.println("\n");
+    jane.buy(1, pretzel);
+    print(jane, pretzel);
 
-    /**
-     * Customer 2 buys 2 of snack 4. Print Customer 2 Cash on Hand. Print quantity
-     * of snack 4.
-     */
     System.out.println("*** Customer 2 buys 2 of snack 4 ***");
-    c2.buy(s4.getTotalCost(2));
-    s4.buySnack(2);
-    System.out.println("Customer 2 Cash on Hand: " + c2.getCashOnHand());
-    System.out.println("Snack 4 Quantity: " + s4.getQuantity());
-    System.out.println("\n");
+    bob.buy(2, soda);
+    print(bob, soda);
     
-    /**
-     * Customer 1 finds $10. Print Customer 1 Cash on Hand.
-     */
     System.out.println("*** Customer 1 found $10 ***");
-    c1.addCash(10.00);
-    System.out.println("Customer 1 Cash on Hand: " + c1.getCashOnHand());
-    System.out.println("\n");
+    jane.addCash(10.00);
+    print(jane, null);
     
-    /**
-     * Customer 1 buys 1 of snack 2. Print Customer 1 Cash on Hand. Print quantity
-     * of snack 2.
-     */
     System.out.println("*** Customer 1 buys 1 of snack 2 ***");
-    c1.buy(s2.getTotalCost(1));
-    s2.buySnack(1);
-    System.out.println("Customer 1 Cash on Hand: " + c1.getCashOnHand());
-    System.out.println("Snack 2 Quantity: " + s2.getQuantity());
-    System.out.println("\n");
-    
-    /**
-     * Snack 3 gets 12 more. Print quantity of snack 3.
-     */
+    jane.buy(1, chocolateBar);
+    print(jane, chocolateBar);
+
     System.out.println("*** Snack 3 gets 12 more ***");
-    s3.addQuantity(12);
-    System.out.println("Snack 3 Quantity: " + s3.getQuantity());
-    System.out.println("\n");
+    pretzel.addQuantity(12);
+    print(null, pretzel);
     
-    /**
-     * Customer 2 buys 3 of snack 3. Print Customer 2 Cash on hand. Print quantity
-     * of snack 3
-     */
     System.out.println("*** Customer 2 buys 3 of snack 3 ***");
-    c2.buy(s3.getTotalCost(3));
-    s3.buySnack(3);
-    System.out.println("Customer 2 Cash on Hand: " + c2.getCashOnHand());
-    System.out.println("Snack 3 Quantity: " + s3.getQuantity());
-    System.out.println("\n");
+    bob.buy(3, pretzel);
+    print(bob, pretzel);
   }
 
   public static void main(String[] args) {
